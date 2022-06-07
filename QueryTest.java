b7ccf0524ddc791c9aa7cecabebfbe141414c453
gaurav-kaushik123 <102759460+gaurav-kaushik123@users.noreply.github.com>
2022-06-07 11:17:54 +0530

    Update QueryTest.java

--- QueryTest.java
+++ QueryTest.java
@@ -1,3 +1,4 @@
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,42 +16,53 @@ public class QueryTest {
 			
 			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system", "sysgitc");
 
-			Statement stmt = conn.createStatement();
+			conn.setAutoCommit(false);
+			//Statement stmt = conn.createStatement();
 			
 			Scanner scan1 = new Scanner(System.in);
 			Scanner scan2 = new Scanner(System.in);
 			Scanner scan3 = new Scanner(System.in);
 			
-			System.out.println("Enter new emp salary : " );
-			double empSalary = scan2.nextDouble();
-			
-			System.out.println("Enter new emp job    : " );
-			String empJob = scan3.nextLine(); 
-			
-			System.out.println("Enter new emp number : " );
+			System.out.println("Enter emp number : " );
 			int empNumber = scan1.nextInt();
 			
-			ResultSet resultSet = stmt.executeQuery("SELECT * FROM emp"
-					+ " where empno="+empNumber);
+			Statement stmt = conn.createStatement();
+			ResultSet resultSet = stmt.executeQuery("select * from emp where empno="+empNumber);
 			
-			if(resultSet.getRow()==0)
+			if(!resultSet.next())
 			{
 				throw new EmpNotExist("The employee with the given number does not exist.");
 			}
 			
 			else
 			{
-			PreparedStatement prstmt = conn.prepareStatement("UPDATE EMP SET SAL=? and JOB=? WHERE EMPNO=?");
 				
+				System.out.println("Enter new emp job    : " );
+				String empJob = scan2.next();
+				
+			System.out.println("Enter new emp salary : " );
+			double empSalary = scan3.nextDouble();
+			
+			 
+			
+			PreparedStatement prstmt = conn.prepareStatement("UPDATE EMP SET SAL=?, JOB=? WHERE EMPNO=?");
+				
+			
 			
 			prstmt.setDouble(1, empSalary);
 			prstmt.setString(2, empJob);
 			prstmt.setInt(3, empNumber);
 			
+			
 			prstmt.executeUpdate();
 			prstmt.close();
-			}
 			
+			conn.commit();
+			
+			System.out.println("Updated successfully....");
+			
+			}
+			resultSet.close();
 			stmt.close();
 			 conn.close(); 
 			
@@ -61,6 +73,10 @@ public class QueryTest {
 	}
 
 }
+
+
+
+
 
 
 
